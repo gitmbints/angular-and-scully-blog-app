4d862d2b3aec6cbb732c55c4240b368f4f2ca496
Ambinintsoa <rahmbints@gmail.com>
2024-01-25 17:05:38 +0300

    Finish my-blog app from Angular Pojects 3rd edition packt book

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactComponent } from './contact/contact.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // Route configuration object
+  { path: 'contact', component: ContactComponent },
+  {
+    path: 'articles',
+    loadChildren: () =>
+      import('./articles/articles.module').then((m) => m.ArticlesModule),
+  },
+  {
+    path: 'posts',
+    loadChildren: () =>
+      import('./posts/posts.module').then((m) => m.PostsModule),
+  },
+  { path: '', pathMatch: 'full', redirectTo: 'articles' },
+  { path: '**', redirectTo: 'articles' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
